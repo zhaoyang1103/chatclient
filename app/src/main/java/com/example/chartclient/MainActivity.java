27b280864ea800cd3c59c0f3b0a071f26cc5b67f
wzy <1084429061@qq.com>
2019-03-31 20:07:55 +0800

    e

--- app/src/main/java/com/example/chartclient/MainActivity.java
+++ app/src/main/java/com/example/chartclient/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.chartclient;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -42,6 +48,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Timer timer;
     private Random random;
     private int a;
+    private Toolbar toolbar;
 
 
     @Override
@@ -56,11 +63,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         text = (EditText) findViewById(R.id.text);
         bt_send = (Button) findViewById(R.id.bt_send);
         lin = (LinearLayout) findViewById(R.id.lin);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setOnClickListener(this);
+        toolbar.setTitle("世界聊天");
+        toolbar.setTitleTextColor(Color.BLACK);
         requestQueue = Volley.newRequestQueue(MainActivity.this);
         lv_list = (ListView) findViewById(R.id.lv_list);
         bt_send.setOnClickListener(this);
         list = new ArrayList<>();
         adapter = new Lv_Adapter();
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         lv_list.setAdapter(adapter);
         timer = new Timer();
         random = new Random();
@@ -71,7 +84,75 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 getMessage();
             }
         }, 0, 3000);
+        final MyDialog myDialog = new MyDialog();
+        myDialog.showMoneyDialog_kong();
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.jia:
+                        myDialog.show();
+                        break;
+                }
+                return false;
+            }
+        });
+
     }
+
+    class MyDialog {
+        AlertDialog.Builder builder;
+
+        public void showMoneyDialog_kong() {
+            builder = new AlertDialog.Builder(MainActivity.this);
+            View view = View.inflate(MainActivity.this, R.layout.dialogview, null);
+            builder.setView(view);
+            builder.setNegativeButton("狠心取消", null);
+            final ViewHolder viewHolder = new ViewHolder(view);
+            viewHolder.tx_weixn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    viewHolder.image_qina.setImageResource(R.drawable.kongweixin);
+                    viewHolder.tx_weixn.setBackgroundResource(R.drawable.weixinback);
+                    viewHolder.tx_zhifubao.setBackgroundColor(Color.parseColor("#FF2196F3"));
+
+                }
+            });
+            viewHolder.tx_zhifubao.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    viewHolder.image_qina.setImageResource(R.drawable.zhifubao);
+                    viewHolder.tx_weixn.setBackgroundColor(Color.parseColor("#FF4CAF50"));
+                    viewHolder.tx_zhifubao.setBackgroundResource(R.drawable.zhifubaoback);
+
+                }
+            });
+
+
+        }
+
+        public void show() {
+            builder.show();
+        }
+
+
+        public
+        class ViewHolder {
+            public View rootView;
+            public ImageView image_qina;
+            public TextView tx_weixn;
+            public TextView tx_zhifubao;
+
+            public ViewHolder(View rootView) {
+                this.rootView = rootView;
+                this.image_qina = (ImageView) rootView.findViewById(R.id.image_qina);
+                this.tx_weixn = (TextView) rootView.findViewById(R.id.tx_weixn);
+                this.tx_zhifubao = (TextView) rootView.findViewById(R.id.tx_zhifubao);
+            }
+
+        }
+    }
+
 
     private void getMessage() {
         JSONObject jsonObject = new JSONObject();
@@ -115,13 +196,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Toast.makeText(this, "聊天框为空", Toast.LENGTH_SHORT).show();
             return;
         } else {
-
             sendData();
         }
 
         // TODO validate success, do something
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.tooritem, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     private void sendData() {
@@ -142,7 +228,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }, new Response.ErrorListener() {
                 @Override
                 public void onErrorResponse(VolleyError volleyError) {
-                    Toast.makeText(MainActivity.this, "网络可能出小差了噢" , Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "网络可能出小差了噢", Toast.LENGTH_SHORT).show();
                     Log.i("失败", "onErrorResponse: " + volleyError.toString());
                 }
             });
